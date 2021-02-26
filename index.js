92f1de91c478a70c537b821c82423a92ea0f9f6b
duanyytop <duanyytop@gmail.com>
2021-02-26 10:36:49 +0800

    feat: Render nervina labs for pc

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const data = {
   solution3_title: "可交换开放凭证",
   solution3_description:
     "EOP(Exchangeable Open Proof)为电子票证、商品预售、粉丝经济、版权分发、艺术品拍卖等等行业提供支持全生命周期的区块链凭证标准。应用方在不修改原业务核心流程的前提下，机遇区块链构建自己的应用生态。第三方开发者围绕着用户和 EOP 提供服务，为平台和用户带来价值。",
+  production_category: "产品",
   production1_title: "Neuron",
   production1_description: "内置全节点的 Nervos 资产客户端",
   production2_title: "CKB Explorer",
@@ -20,21 +21,28 @@ const data = {
   production3_description: "以太坊兼容层区块浏览器",
   production4_title: "Nervos SDK",
   production4_description: "提供 Java/Go/Js 等 SDK 和技术支持",
+  job_category: "开发职位",
   job1_title: "技术开发总监",
-  job1_description: "",
+  job1_description: "Develop",
   job2_title: "产品运营总监",
-  job2_description: "",
+  job2_description: "Develop",
   job3_title: "Rust 高级开发工程师",
-  job3_description: "",
+  job3_description: "Develop",
   job4_title: "Java / Golang 高级开发工程师",
-  job4_description: "",
+  job4_description: "Develop",
   job5_title: "前端高级工程师",
-  job5_description: "",
+  job5_description: "Develop",
   job6_title: "区块链产品经理",
-  job6_description: "",
+  job6_description: "Develop",
   job7_title: "技术 BD 经理",
-  job7_description: "",
+  job7_description: "Develop",
+  copyright: "Nervina Labs All Rights Reserved.",
 };
+
+function gotoElement(id) {
+  const element = document.getElementById(id);
+  element.scrollIntoView({behavior: "smooth"});
+}
 
 function displayElementValue(id, value) {
   let element = document.getElementById(id);
@@ -47,8 +55,15 @@ function keyToElementId(key) {
   return key.replace("_", "-");
 }
 
+function updateElementValue(key, value) {
+  if (key === "copyright") {
+    return `Copyright © ${new Date().getFullYear()} ${value}`;
+  }
+  return value;
+}
+
 (function displayElements() {
   for (key in data) {
-    displayElementValue(keyToElementId(key), data[key]);
+    displayElementValue(keyToElementId(key), updateElementValue(key, data[key]));
   }
 })();
