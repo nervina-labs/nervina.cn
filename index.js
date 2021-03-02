3123a32b86f99db7ad351ccbbb320db756cbb5d4
duanyytop <duanyytop@gmail.com>
2021-03-02 16:55:44 +0800

    feat: Update job content

--- index.js
+++ index.js
@@ -16,38 +16,18 @@ const data = {
   production2_description: "Nervos CKB 区块数据浏览器",
   production3_description: "Nervos Layer 2 以太坊兼容层区块浏览器",
   production4_description: "提供 Java/Go/Js 等 SDK 和技术开发文档",
-  job1_title: "Rust 高级开发工程师",
-  job1_name: "Rust 高级开发工程师",
-  job1_responsibilities: "1. 在 Nervos CKB 上设计和实现智能合约\n 2. 在 Nervos CKB 生态系统中探索和构建各种 dapp",
-  job1_qualifications: "1. 3年以上的软件开发经验\n 2. 熟练使用 Rust 语言，熟悉 C/C++\n 3. 良好的代码风格和书写习惯，对代码质量有一定追求",
-  job2_title: "市场总监",
-  job2_name: "市场总监",
-  job2_responsibilities:
+  job1_title: "市场总监",
+  job1_name: "市场总监",
+  job1_responsibilities:
     "1. 根据公司发展规划，制定产品市场定位、宣传规划与市场策略\n 2. 负责制定对外 PR，发布会计划以及线上线下活动\n 3. 开拓、建立与维护重点媒体关系，制定危机预警管理及处置计划",
-  job2_qualifications:
+  job1_qualifications:
     "1. 本科及以上学历，拥有五年及以上互联网行业市场宣传香港从业经验，具备一定的行业资源\n 2. 性格开朗，亲和力、沟通能力强，具备较强的市场开拓热情，喜欢有挑战性的工作",
-  job3_title: "商务拓展经理",
-  job3_name: "商务拓展经理",
-  job3_responsibilities:
+  job2_title: "商务拓展经理",
+  job2_name: "商务拓展经理",
+  job2_responsibilities:
     "1. 负责与多个垂直领域的互联网企业沟通，尝试将公司解决方案与对方业务进行落地结合\n 2. 负责进行商务洽谈、技术衔接并达成最终合作\n 3. 重要客户问题的收集、反馈并解决\n 4. 维护好重点客户的关系，保证客户的留存率\n 5. 发行新的 BD 对象，拓展业务边界",
-  job3_qualifications:
+  job2_qualifications:
     "1. 本科及以上学历，拥有三年及以上互联网行业商务推广相关从业经验，具有一定的企业客户资源\n 2. 发行新的 BD 对象，拓展业务边界\n 3. 具有较强的独立思考能力，善于分析各方价值点，目标意识强",
-  job4_title: "Java / Golang 高级开发工程师",
-  job4_name: "Java / Golang 高级开发工程师",
-  job4_responsibilities: "1. 在 Nervos CKB 上设计和实现智能合约\n 2. 在 Nervos CKB 生态系统中探索和构建各种 dapp",
-  job4_qualifications: "1. 3年以上的软件开发经验\n 2. 熟练使用 Rust 语言，熟悉 C/C++\n 3. 良好的代码风格和书写习惯，对代码质量有一定追求",
-  job5_title: "前端高级工程师",
-  job5_name: "前端高级工程师",
-  job5_responsibilities: "1. 在 Nervos CKB 上设计和实现智能合约\n 2. 在 Nervos CKB 生态系统中探索和构建各种 dapp",
-  job5_qualifications: "1. 3年以上的软件开发经验\n 2. 熟练使用 Rust 语言，熟悉 C/C++\n 3. 良好的代码风格和书写习惯，对代码质量有一定追求",
-  job6_title: "区块链产品经理",
-  job6_name: "区块链产品经理",
-  job6_responsibilities: "1. 在 Nervos CKB 上设计和实现智能合约\n 2. 在 Nervos CKB 生态系统中探索和构建各种 dapp",
-  job6_qualifications: "1. 3年以上的软件开发经验\n 2. 熟练使用 Rust 语言，熟悉 C/C++\n 3. 良好的代码风格和书写习惯，对代码质量有一定追求",
-  job7_title: "技术 BD 经理",
-  job7_name: "技术 BD 经理",
-  job7_responsibilities: "1. 在 Nervos CKB 上设计和实现智能合约\n 2. 在 Nervos CKB 生态系统中探索和构建各种 dapp",
-  job7_qualifications: "1. 3年以上的软件开发经验\n 2. 熟练使用 Rust 语言，熟悉 C/C++\n 3. 良好的代码风格和书写习惯，对代码质量有一定追求",
   copyright: "Nervina Labs All Rights Reserved.",
 };
 
@@ -98,7 +78,7 @@ function gotoGithub(repository) {
       window.open("https://github.com/nervosnetwork/ckb-sdk-ruby");
       break;
     case "nervina.io":
-      window.open("https://github.com/nervina-labs/nervina.io");
+      window.open("https://github.com/nervina-labs");
       break;
     default:
       break;
