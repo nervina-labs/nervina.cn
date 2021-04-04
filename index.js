68d7ccb5c8e07227ace6f289ba01f439b9b5da80
Dylan <duanyytop@gmail.com>
2021-04-04 17:22:08 +0800

    chore: Update job anchor id

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const data = {
   production2_description: "Nervos CKB 区块数据浏览器",
   production3_description: "Nervos Layer 2 以太坊兼容层区块浏览器",
   production4_description: "提供 Java/Go/JavaScript/Ruby 等 SDK 和技术开发文档",
-  job1_title: "市场总监",
+  job1: "市场总监",
   job1_name: "市场总监",
   job1_responsibilities:
     "1. 根据公司发展规划，制定产品市场定位、宣传规划与市场策略\n 2. 负责制定对外 PR，发布会计划以及线上线下活动\n 3. 开拓、建立与维护重点媒体关系，制定危机预警管理及处置计划",
   job1_qualifications:
     "1. 本科及以上学历，拥有五年及以上互联网行业市场宣传香港从业经验，具备一定的行业资源\n 2. 性格开朗，亲和力、沟通能力强，具备较强的市场开拓热情，喜欢有挑战性的工作",
-  job2_title: "商务拓展经理",
+  job2: "商务拓展经理",
   job2_name: "商务拓展经理",
   job2_responsibilities:
     "1. 负责与多个垂直领域的互联网企业沟通，尝试将公司解决方案与对方业务进行落地结合\n 2. 负责进行商务洽谈、技术衔接并达成最终合作\n 3. 重要客户问题的收集、反馈并解决\n 4. 维护好重点客户的关系，保证客户的留存率\n 5. 发行新的 BD 对象，拓展业务边界",
